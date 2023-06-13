607fbcc3fe4f13b9adb803c1023faf35a498fd49
Anthony Van Cayetano <accayetano4@up.edu.ph>
2023-06-13 17:57:39 +0800

    Implemented prec-recall curve and AUC

--- src/evaluate_clusters.py
+++ src/evaluate_clusters.py
@@ -1,7 +1,7 @@
 # pyright: basic
 
 import time
-from typing import Dict, List, Literal, NotRequired, Set, TypedDict, Union
+from typing import Dict, List, Literal, NotRequired, Set, Tuple, TypedDict, Union
 
 import polars as pl
 from sklearn.metrics import auc
@@ -90,12 +90,15 @@ class ClusterEvaluator:
 
         self.methods = ["unweighted"] + self.feat_methods + self.sv_methods
 
-        self.cache_eval_data()
-
         # to track the progress
         self.idx = 0
         self.total = (
-            len(self.inflations) * (n_dens + 1) * n_iters * 2 * len(self.methods) * 2
+            len(self.inflations)
+            * (n_dens + 1)
+            * n_iters
+            * len(self.edges)
+            * len(self.methods)
+            * 2
         )
 
     def get_cluster_filename(
@@ -162,10 +165,10 @@ class ClusterEvaluator:
         ]
 
         feat_clusters: AllFeatClusters = {}
-        feat_weighted: FeatWeighted = {}  # type: ignore
+        feat_weighted: FeatWeighted = {}
 
         sv_clusters: AllSvClusters = {}
-        sv_weighted: SvWeighted = {}  # type: ignore
+        sv_weighted: SvWeighted = {}
 
         unw_clusters: AllUnwClusters = {}
         df_unweighted = pl.read_csv(
@@ -175,10 +178,15 @@ class ClusterEvaluator:
             has_header=False,
         )
 
+        cache_idx = 0
+        max_cache_idx = len(self.inflations) * len(self.edges) * (
+            len(self.sv_methods) * self.n_iters + len(self.feat_methods)
+        ) + len(self.inflations)
+
         for I in self.inflations:
-            feat_clusters[I] = {}  # type: ignore
-            sv_clusters[I] = {}  # type: ignore
-            unw_clusters[I] = {}  # type: ignore
+            feat_clusters[I] = {}
+            sv_clusters[I] = {}
+            unw_clusters[I] = {}
 
             for e in self.edges:
                 feat_clusters[I][e] = {}
@@ -211,6 +219,8 @@ class ClusterEvaluator:
                                 clusters,
                             )
                         )
+                        cache_idx += 1
+                        print(f"[{cache_idx}/{max_cache_idx}] Done caching")
 
                 for m in self.feat_methods:
                     feat_weighted[e][m] = pl.read_csv(
@@ -232,6 +242,8 @@ class ClusterEvaluator:
                             clusters,
                         )
                     )
+                    cache_idx += 1
+                    print(f"[{cache_idx}/{max_cache_idx}] Done caching")
 
             clusters = get_clusters_list(self.get_cluster_filename("", "unweighted", I))
             unw_clusters[I] = list(
@@ -243,6 +255,8 @@ class ClusterEvaluator:
                     clusters,
                 )
             )
+            cache_idx += 1
+            print(f"[{cache_idx}/{max_cache_idx}] Done caching")
 
         self.train_complexes = train_complexes
         self.test_complexes = test_complexes
@@ -442,7 +456,7 @@ class ClusterEvaluator:
 
     def prec_recall_curves(self):
         """
-        Four precision-recall curves per inflation setting
+        Four precision-recall curves (average of all inflation settings)
         - all_edges, match_thresh = 0.5
         - all_edges, match_thresh = 0.75
         - 20k_edges, match_thresh = 0.5
@@ -450,47 +464,62 @@ class ClusterEvaluator:
         """
         df_cluster_evals = pl.read_csv("../data/evals/cluster_evals.csv")
 
-        curves = {}
-        auc_vals = {}
+        df_all_050 = self.get_prec_recall_curve(df_cluster_evals, "all_edges", 0.5)
+        df_all_075 = self.get_prec_recall_curve(df_cluster_evals, "all_edges", 0.75)
+        df_20k_050 = self.get_prec_recall_curve(df_cluster_evals, "20k_edges", 0.5)
+        df_20k_075 = self.get_prec_recall_curve(df_cluster_evals, "20k_edges", 0.75)
 
-        for inflation in self.inflations:
-            curves[inflation] = {}
+        df_all_050_auc = self.get_prec_recall_auc(df_all_050)
+        df_all_075_auc = self.get_prec_recall_auc(df_all_075)
+        df_20k_050_auc = self.get_prec_recall_auc(df_20k_050)
+        df_20k_075_auc = self.get_prec_recall_auc(df_20k_075)
 
-            df_20k_050 = (
-                df_cluster_evals.lazy()
-                .filter(
-                    (pl.col(INFLATION) == inflation)
-                    & (pl.col(N_EDGES) == "20k_edges")
-                    & (pl.col(MATCH_THRESH) == 0.5)
-                )
-                .select(pl.exclude([INFLATION, N_EDGES, MATCH_THRESH, XVAL_ITER]))
-                .groupby([METHOD, DENS_THRESH])
-                .mean()
-                .sort([METHOD, DENS_THRESH])
-                .collect()
+        print(df_all_050_auc)
+        print(df_all_075_auc)
+        print(df_20k_050_auc)
+        print(df_20k_075_auc)
+
+    def get_prec_recall_curve(
+        self, df_cluster_evals: pl.DataFrame, n_edges: str, match_thresh: float
+    ) -> pl.DataFrame:
+        df_prec_recall = (
+            df_cluster_evals.lazy()
+            .filter(
+                (pl.col(N_EDGES) == n_edges) & (pl.col(MATCH_THRESH) == match_thresh)
             )
+            .groupby([INFLATION, METHOD, DENS_THRESH])
+            .mean()
+            .groupby([METHOD, DENS_THRESH])
+            .mean()
+            .sort([METHOD, DENS_THRESH])
+            .collect()
+        )
 
-            df_20k_050_auc = (
-                df_20k_050.lazy()
-                .groupby(METHOD, maintain_order=True)
-                .agg(
-                    pl.struct([PREC, RECALL])
-                    .apply(
-                        lambda prec_recall: auc(
-                            prec_recall.struct.field(RECALL),
-                            prec_recall.struct.field(PREC),
-                        )
+        return df_prec_recall
+
+    def get_prec_recall_auc(self, df_prec_recall: pl.DataFrame) -> pl.DataFrame:
+        df_auc = (
+            df_prec_recall.lazy()
+            .groupby(METHOD, maintain_order=True)
+            .agg(
+                pl.struct([PREC, RECALL])
+                .apply(
+                    lambda prec_recall: auc(
+                        prec_recall.struct.field(RECALL),
+                        prec_recall.struct.field(PREC),
                     )
-                    .alias(AUC)
                 )
-                .sort(AUC, descending=True)
-                .collect()
+                .alias(AUC)
             )
+            .sort(AUC, descending=True)
+            .collect()
+        )
 
-            print(df_20k_050_auc)
+        return df_auc
 
     def main(self, re_eval: bool = True):
         if re_eval:
+            self.cache_eval_data()
             self.evaluate_complex_prediction()
 
         self.prec_recall_curves()
@@ -498,15 +527,15 @@ class ClusterEvaluator:
 
 if __name__ == "__main__":
     pl.Config.set_tbl_cols(30)
-    pl.Config.set_tbl_rows(15)
+    pl.Config.set_tbl_rows(21)
     start = time.time()
-    inflations = [4]
+    inflations = [3, 4]
     edges = ["20k_edges", "all_edges"]
     feat_methods = [
         f.lower() for f in FEATURES + list(map(lambda sf: sf["name"], SUPER_FEATS))
     ]
     sv_methods = ["cnb", "rf", "mlp", "swc"]
-    n_dens = 10
+    n_dens = 5
     n_iters = 2
 
     cluster_eval = ClusterEvaluator(
