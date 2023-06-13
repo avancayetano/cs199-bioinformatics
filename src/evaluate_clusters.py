0b3cca2caac04a7f5bf847aa13f6b8216701fb74
Anthony Van Cayetano <accayetano4@up.edu.ph>
2023-06-13 14:41:51 +0800

    Implemented cluster density caching

--- src/evaluate_clusters.py
+++ src/evaluate_clusters.py
@@ -1,5 +1,6 @@
 # pyright: basic
 
+import time
 from typing import Dict, List, Literal, NotRequired, Set, TypedDict, Union
 
 import polars as pl
@@ -24,20 +25,24 @@ DENS_THRESH = "DENS_THRESH"
 AUC = "AUC"
 
 Subgraphs = List[Set[str]]
+ScoredCluster = TypedDict(
+    "ScoredCluster", {"COMP_PROTEINS": Set[str], "DENSITY": float}
+)
+ScoredClusters = List[ScoredCluster]
 
 FeatClusters = TypedDict(
     "FeatClusters",
     {
-        "20k_edges": Dict[str, Subgraphs],
-        "all_edges": Dict[str, Subgraphs],
+        "20k_edges": Dict[str, ScoredClusters],
+        "all_edges": Dict[str, ScoredClusters],
     },
 )
 
 SvClusters = TypedDict(
     "SvClusters",
     {
-        "20k_edges": Dict[str, Dict[int, Subgraphs]],
-        "all_edges": Dict[str, Dict[int, Subgraphs]],
+        "20k_edges": Dict[str, Dict[int, ScoredClusters]],
+        "all_edges": Dict[str, Dict[int, ScoredClusters]],
     },
 )
 
@@ -45,7 +50,7 @@ SvClusters = TypedDict(
 # the keys are MCL inflation parameter settings
 AllFeatClusters = Dict[int, FeatClusters]
 AllSvClusters = Dict[int, SvClusters]
-AllUnwClusters = Dict[int, Subgraphs]
+AllUnwClusters = Dict[int, ScoredClusters]
 
 
 FeatWeighted = TypedDict(
@@ -122,7 +127,33 @@ class ClusterEvaluator:
             return f"../data/weighted/{n_edges}/cross_val/{method}{suffix}{iter}.csv"
         return f"../data/weighted/{n_edges}/features/{method}{suffix}.csv"
 
+    def cluster_density(self, df_w: pl.DataFrame, cluster: Set[str]) -> float:
+        if len(cluster) <= 1:
+            return 0
+        sorted_cluster = list(sorted(cluster))
+        df_pairs = pl.DataFrame(
+            [
+                [u, v]
+                for i, u in enumerate(sorted_cluster)
+                for v in sorted_cluster[i + 1 :]
+            ],
+            schema=[PROTEIN_U, PROTEIN_V],
+        )
+        assert_prots_sorted(df_pairs)
+
+        weight = (
+            df_w.join(df_pairs, on=[PROTEIN_U, PROTEIN_V], how="inner")
+            .select(WEIGHT)
+            .to_series()
+            .sum()
+        )
+
+        n = len(cluster)
+        density = 2 * weight / (n * (n - 1))
+        return density
+
     def cache_eval_data(self):
+        print("Caching necessary eval data (might take a while)...")
         train_complexes: List[Subgraphs] = [
             get_complexes_list(xval_iter, "train") for xval_iter in range(self.n_iters)
         ]
@@ -160,20 +191,28 @@ class ClusterEvaluator:
                     sv_clusters[I][e][m] = {}
                     sv_weighted[e][m] = {}
                     for j in range(self.n_iters):
-                        sv_clusters[I][e][m][j] = get_clusters_list(
-                            self.get_cluster_filename(e, m, I, f"_iter{j}")
-                        )
                         sv_weighted[e][m][j] = pl.read_csv(
                             self.get_weighted_filename(e, m, f"_iter{j}"),
                             new_columns=[PROTEIN_U, PROTEIN_V, WEIGHT],
                             separator="\t",
                             has_header=False,
                         )
+                        clusters = get_clusters_list(
+                            self.get_cluster_filename(e, m, I, f"_iter{j}")
+                        )
+                        sv_clusters[I][e][m][j] = list(
+                            map(
+                                lambda cluster: {
+                                    "COMP_PROTEINS": cluster,
+                                    "DENSITY": self.cluster_density(
+                                        sv_weighted[e][m][j], cluster
+                                    ),
+                                },
+                                clusters,
+                            )
+                        )
 
                 for m in self.feat_methods:
-                    feat_clusters[I][e][m] = get_clusters_list(
-                        self.get_cluster_filename(e, m, I)
-                    )
                     feat_weighted[e][m] = pl.read_csv(
                         self.get_weighted_filename(e, m),
                         new_columns=[PROTEIN_U, PROTEIN_V, WEIGHT],
@@ -181,8 +220,28 @@ class ClusterEvaluator:
                         has_header=False,
                     )
 
-            unw_clusters[I] = get_clusters_list(
-                self.get_cluster_filename("", "unweighted", I)
+                    clusters = get_clusters_list(self.get_cluster_filename(e, m, I))
+                    feat_clusters[I][e][m] = list(
+                        map(
+                            lambda cluster: {
+                                "COMP_PROTEINS": cluster,
+                                "DENSITY": self.cluster_density(
+                                    feat_weighted[e][m], cluster
+                                ),
+                            },
+                            clusters,
+                        )
+                    )
+
+            clusters = get_clusters_list(self.get_cluster_filename("", "unweighted", I))
+            unw_clusters[I] = list(
+                map(
+                    lambda cluster: {
+                        "COMP_PROTEINS": cluster,
+                        "DENSITY": self.cluster_density(df_unweighted, cluster),
+                    },
+                    clusters,
+                )
             )
 
         self.train_complexes = train_complexes
@@ -262,7 +321,7 @@ class ClusterEvaluator:
             evals.extend([metrics_050, metrics_075])
             self.idx += 2
             print(
-                f"[{self.idx}/{self.total}]Done evaluating {method} clusters on {n_edges}. dens_thresh={dens_thresh}. xval_iter={xval_iter}"
+                f"[{self.idx}/{self.total}] Done evaluating {method} clusters on {n_edges}. dens_thresh={dens_thresh}. xval_iter={xval_iter}"
             )
 
         return evals
@@ -285,27 +344,6 @@ class ClusterEvaluator:
                 return True
         return False
 
-    def cluster_density(self, df_w: pl.DataFrame, cluster: Set[str]) -> float:
-        sorted_cluster = list(sorted(cluster))
-        df_pairs = pl.DataFrame(
-            [
-                [u, v]
-                for i, u in enumerate(sorted_cluster)
-                for v in sorted_cluster[i + 1 :]
-            ],
-            schema=[PROTEIN_U, PROTEIN_V],
-        )
-        assert_prots_sorted(df_pairs)
-        weight = (
-            df_w.join(df_pairs, on=[PROTEIN_U, PROTEIN_V], how="inner")
-            .select(WEIGHT)
-            .to_series()
-            .sum()
-        )
-        n = len(cluster)
-        density = 2 * weight / (n * (n - 1))
-        return density
-
     def get_complex_prediction_metrics(
         self,
         inflation: int,
@@ -316,24 +354,24 @@ class ClusterEvaluator:
         match_thresh: float,
     ) -> Dict[str, Union[str, float, int]]:
         if method in self.sv_methods:
-            clusters = self.sv_clusters[inflation][n_edges][method][xval_iter]
-            df_weighted = self.sv_weighted[n_edges][method][xval_iter]
+            scored_clusters = self.sv_clusters[inflation][n_edges][method][xval_iter]
         elif method in self.feat_methods:
-            clusters = self.feat_clusters[inflation][n_edges][method]
-            df_weighted = self.feat_weighted[n_edges][method]
+            scored_clusters = self.feat_clusters[inflation][n_edges][method]
         else:  # for method == unweighted
-            clusters = self.unw_clusters[inflation]
-            df_weighted = self.df_unweighted
+            scored_clusters = self.unw_clusters[inflation]
 
         train_complexes = self.train_complexes[xval_iter]
         test_complexes = self.test_complexes[xval_iter]
 
         # Get only the reliable clusters
         clusters = list(
-            filter(
-                lambda cluster: len(cluster) >= 2
-                and self.cluster_density(df_weighted, cluster) >= dens_thresh,
-                clusters,
+            map(
+                lambda scored_cluster: scored_cluster["COMP_PROTEINS"],
+                filter(
+                    lambda scored_cluster: len(scored_cluster["COMP_PROTEINS"]) >= 2
+                    and scored_cluster["DENSITY"] >= dens_thresh,
+                    scored_clusters,
+                ),
             )
         )
 
@@ -418,11 +456,11 @@ class ClusterEvaluator:
         for inflation in self.inflations:
             curves[inflation] = {}
 
-            df_all_050 = (
+            df_20k_050 = (
                 df_cluster_evals.lazy()
                 .filter(
                     (pl.col(INFLATION) == inflation)
-                    & (pl.col(N_EDGES) == "all_edges")
+                    & (pl.col(N_EDGES) == "20k_edges")
                     & (pl.col(MATCH_THRESH) == 0.5)
                 )
                 .select(pl.exclude([INFLATION, N_EDGES, MATCH_THRESH, XVAL_ITER]))
@@ -432,8 +470,9 @@ class ClusterEvaluator:
                 .collect()
             )
 
-            df_all_050_auc = (
-                df_all_050.groupby(METHOD, maintain_order=True)
+            df_20k_050_auc = (
+                df_20k_050.lazy()
+                .groupby(METHOD, maintain_order=True)
                 .agg(
                     pl.struct([PREC, RECALL])
                     .apply(
@@ -445,9 +484,10 @@ class ClusterEvaluator:
                     .alias(AUC)
                 )
                 .sort(AUC, descending=True)
+                .collect()
             )
 
-            print(df_all_050_auc)
+            print(df_20k_050_auc)
 
     def main(self, re_eval: bool = True):
         if re_eval:
@@ -458,7 +498,8 @@ class ClusterEvaluator:
 
 if __name__ == "__main__":
     pl.Config.set_tbl_cols(30)
-
+    pl.Config.set_tbl_rows(15)
+    start = time.time()
     inflations = [4]
     edges = ["20k_edges", "all_edges"]
     feat_methods = [
@@ -476,4 +517,6 @@ if __name__ == "__main__":
         n_dens=n_dens,
         n_iters=n_iters,
     )
-    cluster_eval.main(re_eval=False)
+
+    cluster_eval.main(re_eval=True)
+    print(f"Execution Time: {time.time() - start}")
